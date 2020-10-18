875f53e9570fb8961d8ab10645aa4cc31cde0616
m4gie <steeven.regnault@epitech.eu>
2020-10-18 18:03:18 +0200

    feat(client): navbar for the home section
    #2

---
+++ packages/client/src/components/Header/NavBar.tsx
@@ -0,0 +1,66 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useTheme, useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import NavItems from '../../constant/navBar.json';
+import Hoverable from '../Hoverable';
+import Text from '../Text';
+
+type Props = {
+  currentName: string;
+};
+
+export default function NavBar({ currentName }: Props) {
+  const { colors } = useTheme();
+  const navigation = useNavigation();
+
+  function handlePress(route: string) {
+    navigation.navigate(route);
+  }
+
+  return (
+    <View style={styles.container}>
+      {NavItems.map(({ name, icon, route }) => {
+        const bgColor = route === currentName ? colors.border : colors.primary;
+        return (
+          <Hoverable
+            key={name}
+            style={[styles.navItem, { borderColor: colors.border, backgroundColor: bgColor }]}
+            onHover={{ backgroundColor: colors.border }}
+            onPress={() => handlePress(route)}>
+            <FontAwesome5 name={icon} size={20} color={colors.text} />
+            <Text fontFamily="title" fontSize="xl" style={styles.navText}>
+              {name}
+            </Text>
+          </Hoverable>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRightWidth: 2,
+    borderLeftWidth: 2,
+    borderTopWidth: 2,
+    borderTopStartRadius: 5,
+    borderTopEndRadius: 5,
+    paddingHorizontal: '1rem',
+    paddingVertical: '.25rem',
+    marginRight: '1.5rem',
+    backgroundColor: 'transparent',
+  },
+  navText: {
+    marginLeft: '.5rem',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+  },
+});
