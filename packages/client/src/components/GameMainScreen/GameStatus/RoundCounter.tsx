adbe04856e0b9d0373c9db476b28aa76e765c66b
m4gie <steeven.regnault@epitech.eu>
2020-10-25 14:43:18 +0100

    feat(client): use squiz v1 theme for the roundcounter
    #2

--- packages/client/src/components/GameMainScreen/GameStatus/RoundCounter.tsx
+++ packages/client/src/components/GameMainScreen/GameStatus/RoundCounter.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@react-navigation/native';
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
@@ -5,6 +6,7 @@ import { useSocketListener } from '../../../utils/hooks/socketListener';
 import Text from '../../Text';
 
 export default function RoundCounter() {
+  const { colors } = useTheme();
   const roundCountInfo: null | {
     current: number;
     total: number;
@@ -17,7 +19,12 @@ export default function RoundCounter() {
       {Array.from({ length: roundCountInfo.total }, (v, k) => k).map((value) => {
         return (
           <Text key={value} fontSize="xl">
-            {value >= roundCountInfo.current ? '○' : '●'}
+            <View
+              style={[
+                styles.dot,
+                { backgroundColor: value >= roundCountInfo.current ? colors.border : colors.text },
+              ]}
+            />
           </Text>
         );
       })}
@@ -28,5 +35,12 @@ export default function RoundCounter() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+  },
+  dot: {
+    width: 20,
+    height: 20,
+    borderRadius: 50,
   },
 });
