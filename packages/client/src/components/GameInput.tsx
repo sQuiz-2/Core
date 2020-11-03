d2e62d5ec2eeff265dc7ef75d21848355dabe2e6
m4gie <steeven.regnault@epitech.eu>
2020-11-03 20:41:15 +0100

    feat(client): play a sound for right or wrong answers
    #37

--- packages/client/src/components/GameInput.tsx
+++ packages/client/src/components/GameInput.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@react-navigation/native';
-import React, { useState, createRef } from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import {
   StyleSheet,
   TextInput,
@@ -11,14 +11,32 @@ import { useRecoilValue } from 'recoil';
 
 import { fontSizes, fontFamilies } from '../constant/theme';
 import socketState from '../global/socket';
+import { useSocketListener } from '../utils/hooks/socketListener';
+import { useSound } from '../utils/hooks/sound';
 import Card from './Card/Card';
 import { GameTimer } from './Timer';
+
+type ResultAnswer = {
+  found: boolean;
+};
 
 export default function GameInput() {
   const [playerAnswer, setPlayerAnswer] = useState('');
   const socket = useRecoilValue(socketState);
   const { colors } = useTheme();
   const inputRef = createRef<TextInput>();
+  const resultAnswer: null | ResultAnswer = useSocketListener('result', null);
+  const foundSound = useSound({ source: require('../../assets/sounds/right.mp3') });
+  const wrongSound = useSound({ source: require('../../assets/sounds/wrong.mp3') });
+
+  useEffect(() => {
+    if (!resultAnswer) return;
+    if (resultAnswer.found) {
+      foundSound.play();
+    } else {
+      wrongSound.play();
+    }
+  }, [resultAnswer]);
 
   function emitAnswer() {
     if (socket) {
