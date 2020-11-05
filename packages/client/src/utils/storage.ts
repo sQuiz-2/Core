e2c76cd5cd70b946ba9ce1a501df260161560d43
m4gie <steeven.regnault@epitech.eu>
2020-11-05 18:41:29 +0100

    feat(client): storage functions
    #1

---
+++ packages/client/src/utils/storage.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export enum StorageEnum {
+  User = 'sQuiz-user',
+}
+
+export function setInStore(key: StorageEnum, value: string) {
+  if (!value) return;
+  const jsonValue = JSON.stringify(value);
+  return AsyncStorage.setItem(key, jsonValue);
+}
+
+export async function getFromStore<T>(key: StorageEnum) {
+  const value = await AsyncStorage.getItem(key);
+  if (!value) return null;
+  return JSON.parse(value) as T;
+}
+
+export function removeInStore(key: StorageEnum) {
+  return AsyncStorage.removeItem(key);
+}
