c81f48c1d7ee9b8793eef2c65655bebc050c0984
m4gie <steeven.regnault@epitech.eu>
2020-11-24 10:58:39 +0100

    feat(client): display lifes

---
+++ packages/client/src/screens/Room/GameInProgress/GameInput/Life/LifeStyle.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    width: 10,
+    height: 40,
+    paddingLeft: 5,
+  },
+});
+
+export default styles;
