2dc8183ca52e63c1830b64c82654f431b0e877ae
m4gie <steeven.regnault@epitech.eu>
2020-12-07 15:04:45 +0100

    feat(client): display faq

---
+++ packages/client/src/screens/FAQ/FAQStyle.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  question: {
+    fontWeight: 'bold',
+    paddingBottom: 20,
+    paddingTop: 50,
+  },
+});
+export default styles;
