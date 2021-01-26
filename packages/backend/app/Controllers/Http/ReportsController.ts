0f9351208692576a5bcf23ba54e04e02963010be
m4gie <steeven.regnault@epitech.eu>
2021-01-26 10:47:34 +0100

    fix(backend): remove console.log

--- packages/backend/app/Controllers/Http/ReportsController.ts
+++ packages/backend/app/Controllers/Http/ReportsController.ts
@@ -4,7 +4,6 @@ import Report from 'App/Models/Report';
 export default class ThemesController {
   public async reset(ctx: HttpContextContract) {
     const { id } = ctx.params;
-    console.log(id);
     const report = await Report.findOrFail(id);
     report.delete();
     return report;
