0dfdd3cd3d3969da2dcc6109c6df1d6e86b55c79
Maji <steeven.regnault@outlook.fr>
2021-05-16 14:59:35 +0200

    fix(backend): don't need to convert to camelCase it's in camelCase by default

--- packages/backend/start/orm.ts
+++ packages/backend/start/orm.ts
@@ -11,7 +11,7 @@ class CamelCaseNamingStrategy implements NamingStrategyContract {
   }
 
   public serializedName(_model: typeof BaseModel, propertyName: string) {
-    return string.camelCase(propertyName);
+    return propertyName;
   }
 
   public relationLocalKey(
