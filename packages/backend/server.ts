9f7b7013d7fb6755df032ee82aed4c71ce08c921
Maji <steeven.regnault@outlook.fr>
2021-12-29 11:41:39 +0100

    chore(backend): update adonis packages to latest

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -11,9 +11,9 @@
 */
 
 import 'reflect-metadata';
-import { Ignitor } from '@adonisjs/core/build/src/Ignitor';
 import sourceMapSupport from 'source-map-support';
+import { Ignitor } from '@adonisjs/core/build/standalone';
 
 sourceMapSupport.install({ handleUncaughtExceptions: false });
 
-new Ignitor(__dirname).httpServer().start().catch(console.error);
+new Ignitor(__dirname).httpServer().start();
